f6f3ae94d7138be896f5df08ddb9c0a7fb317eb9
Zuckmayer91 <bz_91.k@hotmail.com>
2020-07-23 20:13:20 -0500

    basededatos

--- src/app/Service/service.service.ts
+++ src/app/Service/service.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import {Persona} from '../Modelo/Persona';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ServiceService {
+export class ServiceService {  
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  Url='http://localhost:8080/crud-Angular/personas';
+  getPersonas(){
+    return this.http.get<Persona[]>(this.Url);
+  }
 }
